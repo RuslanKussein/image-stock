a3735ac0ffad8a0e9aaaf5bfb780d19a9228c4ff
RuslanKussein <ruslankusein@gmail.com>
2020-07-12 00:15:44 +0500

    add userinfo and buttons

--- src/containers/ExpandedImage.js
+++ src/containers/ExpandedImage.js
@@ -3,10 +3,35 @@ import UserInfo from '../components/UserInfo';
 
 class ExpandedImage extends React.Component {
     render() {
-        const {url, data: {user, description, alt_description}} = this.props;
+        const {url, data: {user: {name, username, profile_image: image}, description, alt_description, links}} = this.props;
         const alt_descriptionArray = alt_description ? alt_description.split(' ') : [];
         return (
             <div className="expanded-image-container">
+
+                <div className="expanded-image-container__more">
+                    <div className="expanded-image-container__user">
+                        <img src={image.large} alt="" className="expanded-image-container__user__photo"/>
+                        <div className="expanded-image-container__user__info">
+                            <p className="expanded-image-container__user__info__name">{name}</p>
+                            <p className="expanded-image-container__user__info__username">@{username}</p>
+                        </div>
+                    </div>
+
+                    <div className="expanded-image-container__buttons">
+
+                        <button className="expanded-image-container__buttons__button expanded-image-container__buttons__button_like">
+                            <i className="fas fa-heart"></i>
+                        </button>
+
+                        <a className="expanded-image-container__buttons__button expanded-image-container__buttons__button_download"
+                           href={`${links.download}`}
+                           target="_blank">
+                            <i className="fas fa-arrow-circle-down"></i>
+                        </a>
+
+                    </div>
+                </div>
+
                 <img className="expanded-image-container__image"
                      src={url}
                      alt={description || `No Description`}/>
