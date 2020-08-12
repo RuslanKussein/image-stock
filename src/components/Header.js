f3d3bc1eae92c6bf8fc1d1e5c7d188996c3a418b
RuslanKussein <ruslankusein@gmail.com>
2020-08-12 22:26:46 +0600

    add descent history feature

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,18 @@
 import React, {PureComponent} from 'react';
 import {Link} from "react-router-dom";
 import '../styles/style.css'
+import History from "./History";
+import {setQueryAction} from "../redux/actions/query";
+import {addImageToFavoritesAction, removeImageFromFavoritesAction} from "../redux/actions/favorites";
+import {addToSearchHistoryAction, clearSearchHistoryAction} from "../redux/actions/searchHistory";
+import {connect} from "react-redux";
 
 class Header extends PureComponent {
     constructor(props) {
         super(props);
         this.state = {
             onTop: true,
+            showHistory: false
         };
         this.handleScroll = this.handleScroll.bind(this);
     }
@@ -27,28 +33,41 @@ class Header extends PureComponent {
 
     render() {
         return (
-            <header className="header">
-                <nav className="nav">
-                    <Link to="/">
-                        <button className="nav__button nav__button_home">
-                            <i className="fas fa-camera"/>
+            <>
+                <header className="header">
+                    <nav className="nav">
+                        <Link to="/">
+                            <button className="nav__button nav__button_home">
+                                <i className="fas fa-camera"/>
+                            </button>
+                        </Link>
+                        <button className={`nav__button nav__button_search ${this.state.onTop && "hidden"}`}>
+                            <i className="fas fa-search"/>
                         </button>
-                    </Link>
-                    <button className={`nav__button nav__button_search ${this.state.onTop && "hidden"}`}>
-                        <i className="fas fa-search"/>
-                    </button>
-                    <Link to="/favorites">
-                        <button className="nav__button nav__button_favorites">
-                            <i className="fas fa-heart"/>
+                        <Link to="/favorites">
+                            <button className="nav__button nav__button_favorites">
+                                <i className="fas fa-heart"/>
+                            </button>
+                        </Link>
+                        <button className="nav__button nav__button_search-history" onClick={() => this.setState({showHistory: !this.state.showHistory})}>
+                            <i className="fas fa-history"/>
                         </button>
-                    </Link>
-                    <button className="nav__button nav__button_search-history">
-                        <i className="fas fa-history"/>
-                    </button>
-                </nav>
-            </header>
+                    </nav>
+                </header>
+
+                {
+                    //SHOW HISTORY LOGIC
+                    this.state.showHistory && <History history={this.props.searchHistory}/>
+                }
+            </>
         )
     }
 }
 
-export default Header;
+function mapStateToProps(state) {
+    return {
+        searchHistory: state.searchHistory
+    }
+}
+
+export default connect(mapStateToProps)(Header);
