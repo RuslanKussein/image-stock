de904c3a64be30b7d3b4722bbad6c2245edfa9d6
RuslanKussein <ruslankusein@gmail.com>
2020-08-13 14:02:47 +0600

    add state persistence feature

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,11 @@
 import {createStore} from "redux";
 import root from "./reducers/root";
 import { composeWithDevTools } from 'redux-devtools-extension'
+import {persistStore} from "./storeEnhancers/statePersistence";
 
 const store = createStore(
     root,
-    composeWithDevTools()
+    composeWithDevTools(persistStore)
 );
 
 window.store = store;
